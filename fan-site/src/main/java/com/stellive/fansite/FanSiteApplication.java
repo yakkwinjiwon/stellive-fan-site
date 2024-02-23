4341c40b0e766bd91c44cd7e087b2ba5f56aa8c9
yakkwinjiwon <yakkwinjiwon@gmail.com>
2024-02-23 14:06:18 +0900

    init

---
+++ fan-site/src/main/java/com/stellive/fansite/FanSiteApplication.java
@@ -0,0 +1,13 @@
+package com.stellive.fansite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanSiteApplication.class, args);
+	}
+
+}
