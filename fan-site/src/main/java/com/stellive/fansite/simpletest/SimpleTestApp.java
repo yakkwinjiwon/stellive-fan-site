bd33a352ef13da4a89809641a106f8b1220765b9
yakkwinjiwon <yakkwinjiwon@gmail.com>
2024-03-08 13:45:06 +0900

    .gitignore

---
+++ fan-site/src/main/java/com/stellive/fansite/simpletest/SimpleTestApp.java
@@ -0,0 +1,17 @@
+package com.stellive.fansite.simpletest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Map;
+
+@Profile("simpleTest")
+@SpringBootApplication
+@Slf4j
+public class SimpleTestApp {
+
+    public static void main(String[] args) {
+
+    }
+}
