773536a6c22d81ac7c487e1b7b12cf68668fe370
yakkwinjiwon <yakkwinjiwon@gmail.com>
2024-03-12 18:18:15 +0900

    Twitter Api delete

--- fan-site/src/main/java/com/stellive/fansite/utils/ApiUtils.java
+++ fan-site/src/main/java/com/stellive/fansite/utils/ApiUtils.java
@@ -2,8 +2,6 @@ package com.stellive.fansite.utils;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 @Getter
@@ -14,28 +12,4 @@ public class ApiUtils {
      */
     @Value("${youtube-api-key}")
     private String youtubeApiKey;
-
-    /**
-     * Twitter token
-     */
-    @Value("${twitter-api-key}")
-    private String twitterApiKey;
-
-    @Value("${twitter-api-key-secret}")
-    private String twitterApiKeySecret;
-
-    @Value("${twitter-bearer-token}")
-    private String twitterBearerToken;
-
-    @Value("${twitter-client-id}")
-    private String twitterClientId;
-
-    @Value("${twitter-client-secret}")
-    private String twitterClientSecret;
-
-    @Value("${twitter-access-token}")
-    private String twitterAccessToken;
-
-    @Value("${twitter-access-token-secret}")
-    private String twitterAccessTokenSecret;
 }
