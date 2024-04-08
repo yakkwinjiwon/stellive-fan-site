8322766ce56d4e1ad62275dde8569000cca2853a
yakkwinjiwon <yakkwinjiwon@gmail.com>
2024-04-08 23:08:38 +0900

    test

--- fan-site/src/test/java/com/stellive/fansite/service/ChannelServiceTest.java
+++ fan-site/src/test/java/com/stellive/fansite/service/ChannelServiceTest.java
@@ -4,13 +4,21 @@ import com.stellive.fansite.api.ChannelFetcher;
 import com.stellive.fansite.domain.Channel;
 import com.stellive.fansite.domain.News;
 import com.stellive.fansite.domain.StellaChannel;
+import com.stellive.fansite.dto.playlistitem.PlaylistItemList;
 import com.stellive.fansite.external.NewsScraper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.web.client.RestClientException;
 
+import java.net.URI;
 import java.util.List;
+
+import static com.stellive.fansite.utils.YoutubeApiConst.DELAY;
+import static com.stellive.fansite.utils.YoutubeApiConst.MAX_ATTEMPTS;
 
 @Slf4j
 @SpringBootTest
@@ -34,5 +42,13 @@ class ChannelServiceTest {
 
     @Test
     void updateChannel() {
+        callApi();
+    }
+
+    @Retryable(value = {RestClientException.class}, maxAttempts = MAX_ATTEMPTS,
+            backoff = @Backoff(delay = DELAY))
+    public void callApi() {
+        log.info("callApi");
+        throw new RestClientException("Error");
     }
 }
